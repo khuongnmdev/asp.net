a690115ccdb38e56c77fc4cd52ba3d104ea11fdc
khuongnm.dev <khuongnm.dev@gmail.com>
2024-07-13 15:11:53 +0700

    Update KhachHang route and DB value

--- QuanLyQuanCafe/Models/KhachHang.cs
+++ QuanLyQuanCafe/Models/KhachHang.cs
@@ -4,13 +4,19 @@ namespace QuanLyQuanCafe.Models
 {
     public class KhachHang
     {
+        [Display(Name = "Mã Khách Hàng")]
         public int ID { get; set; }
+        [Display(Name = "Tên Khách Hàng")]
         public string TenKhachHang { get; set; } = string.Empty;
+        [Display(Name = "Điện Thoại")]
         public string DienThoai { get; set; } = string.Empty;
         public string Email { get; set; } = string.Empty;
+        [Display(Name = "Địa Chỉ")]
         public string DiaChi { get; set; } = string.Empty;
+        [Display(Name = "Giới Tính")]
         public string GioiTinh { get; set; } = string.Empty;
 
+        [Display(Name = "Ngày Sinh")]
         [DataType(DataType.Date)]
         public DateTime NgaySinh { get; set; }
     }
